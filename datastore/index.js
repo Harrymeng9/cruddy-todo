1e2332c557e13c63d596935be271d4265d214f8e
Ghasb001 <hasbun.guillermo@gmail.com>
2022-11-09 23:43:13 -0500

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -8,41 +8,27 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  // var id = counter.getNextUniqueId();
-  // items[id] = text;
-  // // console.log('CALLBACK', callback);
-  // callback(null, { id, text });
   counter.getNextUniqueId((err, data) => {
     fs.writeFile(path.join(exports.dataDir, `${data}.txt`), text, (err) => {
       if (err) {
         throw new Error('ERROR')
       } else {
-        //console.log('TEXT', text);
         callback(null, { id: data, text: text });
       }
-    })
-  })
+    });
+  });
 };
 
 exports.readAll = (callback) => {
 
-  //console.log('items', items);
-  //readdir
   fs.readdir(exports.dataDir, (err, files) => {
-    //console.log(path.join(exports.dataDir, `.txt`))
     if (err) {
       throw new Error('ERROR');
     } else {
-      //var array = [];
       var data = _.map(files, (file) => {
-        //console.log('FILES', files); // Output: [ '00001.txt', '00002.txt' ]
-        // console.log('FILES', fs.readFile(file, callback)); // Output: [ '00001.txt', '00002.txt' ]
-        // expected { 'id' : '00001', 'text': 'todo 1'}
-        var input = file.split('.')[0]
-        var fileSave = { id: input, text: input }
-        //array.push({id, text});
+        var input = file.split('.')[0];
+        var fileSave = { id: input, text: input };
         return fileSave;
-        // return { id, text };
       });
       callback(null, data);
     }
@@ -52,12 +38,9 @@ exports.readAll = (callback) => {
 exports.readOne = (id, callback) => {
 
   var path = `${exports.dataDir}/${id}.txt`;
-  // console.log('PATH', path);
-  // fs.readdir(exports.dataDir, (err, files) => {
   fs.readFile(path, (err, data) => {
     if (err || parseInt(id) === NaN) {
-      //throw new Error('ERROR');
-      callback(new Error ('ERROR'));
+      callback(new Error('ERROR'));
     } else {
       callback(null, { id, text: data.toString() });
     }
@@ -77,21 +60,22 @@ exports.update = (id, text, callback) => {
         } else {
           callback(null, { id, text });
         }
-      })
+      });
     }
-  })
+  });
 
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  var path = `${exports.dataDir}/${id}.txt`;
+
+  fs.unlink(path, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
