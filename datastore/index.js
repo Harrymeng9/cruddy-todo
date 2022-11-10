1d19c87228d1a222740cdbdd7db5845ba4b10b67
Ghasb001 <hasbun.guillermo@gmail.com>
2022-11-09 23:30:26 -0500

    Complete updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -65,13 +65,22 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+
+  var path = `${exports.dataDir}/${id}.txt`;
+  fs.exists(path, function (exists) {
+    if (!exists) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(path, text, (err) => {
+        if (err) {
+          callback(new Error(`No item with id: ${id}`));
+        } else {
+          callback(null, { id, text });
+        }
+      })
+    }
+  })
+
 };
 
 exports.delete = (id, callback) => {
